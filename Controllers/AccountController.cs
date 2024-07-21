17884cd841f8b28b3ebc73a21514e70f7d41c7fe
qiuxan <qiuxan@gmail.com>
2024-07-21 13:09:29 +1000

    add Registrerition End point

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,90 @@
+﻿using AuthenticationDemo.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthenticationDemo.Controllers
+{
+    public class AccountController(UserManager<AppUser> userManager, IConfiguration configuration)
+        : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] AddOrUpdateAppUserModel model) 
+        {
+
+            // Check if the model is valid
+
+            if (ModelState.IsValid) 
+            {
+                var existedUser = await userManager.FindByNameAsync(model.UserName);
+                if (existedUser != null)
+                {
+                    ModelState.AddModelError("", "User name is already taken");
+                    return BadRequest(ModelState);
+                }
+                // Create a new user object
+                var user = new AppUser()
+                {
+                    UserName = model.UserName,
+                    Email = model.Email,
+                    SecurityStamp = Guid.NewGuid().ToString()
+                };
+
+                // Try to save the user
+
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                // If the user is saved successfully, return a 201 Created status code
+                if (result.Succeeded)
+                {
+                    var token = GenerateToken(model.UserName);
+                    return Ok(new { token });
+                }
+
+                // If there are any errors, add them to the ModelState object
+                // and return the error to the client
+                foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+            }
+            // If we got this far, something failed, redisplay form
+            return BadRequest(ModelState);
+
+        }
+
+
+        private string? GenerateToken(string userName)
+        {
+            var secret = configuration["JwtConfig:Secret"];
+            var issuer = configuration["JwtConfig:ValidIssuer"];
+            var audience = configuration["JwtConfig:ValidAudiences"];
+            if (secret is null || issuer is null || audience is null)
+            { throw new ApplicationException("Jwt is not set in the configuration"); }
+
+            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                new Claim(ClaimTypes.Name, userName)
+                }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                Issuer = issuer,
+                Audience = audience,
+                SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+            var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+            var token = tokenHandler.WriteToken(securityToken);
+
+            return token;
+
+        }
+    }
+
+
+}
